41de7ac9eb17d5a2409c908e22349b58793ab8a7
victor-prm <victorpabb@gmail.com>
2025-03-24 13:59:38 +0100

    added footer component from old project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './Header'
+import Footer from './Footer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Header cname="navheader"></Header>
+      <Footer cname="navfooter"></Footer>
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { getCurrentPath } from "./helpers";
+
+
+function Footer(props) {
+    const [count, setCount] = useState(0)
+    const cname = props.cname;
+
+    return (
+        <>
+            <footer className={cname}>
+                <nav className={`${cname}__nav`}>
+                    <ul className={`${cname}__link-list`}>
+                        <li className={`${cname}__link-item`}>
+                            <a href="#" className={`${cname}__link`}><i className="fa fa-heart"></i></a>
+                        </li>
+                        <li className={`${cname}__link-item`}>
+                            <a href="#" className={`${cname}__link`}><i className="fas fa-home"></i></a>
+                        </li>
+                        <li className={`${cname}__link-item`}>
+                            <a href="#" className={`${cname}__link`}><i className="fas fa-bell"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
